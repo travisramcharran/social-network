e687f6fbeed09cb439c5b0bc708009dada9df7e7
Travis Ramcharran <tc.ramcharran@gmail.com>
2022-09-20 17:29:59 -0500

    routes set up

---
+++ routes/api/user.routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/user-controller');
+
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router
+    .route('/:userId')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router
+.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend);
+
+
+module.exports = router;
